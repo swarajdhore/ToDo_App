5e0fa0eec9a7ddee8e5960a9f223a32d47d2bbf9
swarajdhore <dhoreswaraj@gmail.com>
2022-04-03 11:23:44 +0530

    Installed the dependencies

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
-const express = require("express");
-const OurApp = express();
+import express from 'express';
+const TodoApp = express();
+const database = require("./database")
 
-OurApp.get("/", (request,response)=>{
+
+TodoApp.get("/", (request,response)=>{
     response.json({message:"Request Served !!"});
 });
 
-OurApp.listen(4000,()=> console.log("Server is running"));
+TodoApp.get("/home",(req,res)=>{
+    res.json({task:database.task})
+});
+
+//OurApp.post("/signup", aync(req,res) =>{
+    // try{
+    //     const {userid, password}
+    // } catch (error){
+    //     return res.status(500).json({error:error.message});
+    // }
+//});
+
+TodoApp.listen(4000,()=> console.log("Server is running"));
